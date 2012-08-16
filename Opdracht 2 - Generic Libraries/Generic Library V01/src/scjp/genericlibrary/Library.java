efde0060e24651c550a8fed96b0ba383dce7307b
Marcel Akkerman <marcelakkerman@stedin.net>
2012-08-16 22:38:53 +0200

    add implementation

--- Opdracht 2 - Generic Libraries/Generic Library V01/src/scjp/genericlibrary/Library.java
+++ Opdracht 2 - Generic Libraries/Generic Library V01/src/scjp/genericlibrary/Library.java
@@ -1,26 +1,33 @@
 package scjp.genericlibrary;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Library {
+	Map<String, Lendable> available = new HashMap<String, Lendable>();
+	Map<String, Lendable> due = new HashMap<String, Lendable>();
+
 	public Lendable lendItem(String libraryCode) {
-		// TODO implement lendItem(String)
-		return null;
+		Lendable item = available.get(libraryCode);
+		if (item != null) {
+			available.remove(item.getLibraryCode());
+			due.put(item.getLibraryCode(), item);
+		}
+
+		return item;
 	}
 
 	public void returnItem(Lendable item) {
-		// TODO implement lendItem(Lendable)
+		due.remove(item.getLibraryCode());
+		available.put(item.getLibraryCode(), item);
 	}
 
-	// TODO remove warning by adding generics
-	public Collection availableItems() {
-		// TODO implement availableItems()		
-		return null;
+	public Collection<Lendable> availableItems() {
+		return available.values();
 	}
 
-	// TODO remove warning by adding generics
-	public Collection dueItems() {
-		// TODO implement dueItems()
-		return null;
+	public Collection<Lendable> dueItems() {
+		return due.values();
 	}
 }
