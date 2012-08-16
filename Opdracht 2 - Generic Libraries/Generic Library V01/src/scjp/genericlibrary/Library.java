3df2777ff7b15e2c6aca3c5cae4b2b11a158b897
Marcel Akkerman <marcelakkerman@stedin.net>
2012-08-16 22:34:40 +0200

    add Generic Library V01 - reference implementation

---
+++ Opdracht 2 - Generic Libraries/Generic Library V01/src/scjp/genericlibrary/Library.java
@@ -0,0 +1,26 @@
+package scjp.genericlibrary;
+
+import java.util.Collection;
+
+public class Library {
+	public Lendable lendItem(String libraryCode) {
+		// TODO implement lendItem(String)
+		return null;
+	}
+
+	public void returnItem(Lendable item) {
+		// TODO implement lendItem(Lendable)
+	}
+
+	// TODO remove warning by adding generics
+	public Collection availableItems() {
+		// TODO implement availableItems()		
+		return null;
+	}
+
+	// TODO remove warning by adding generics
+	public Collection dueItems() {
+		// TODO implement dueItems()
+		return null;
+	}
+}
