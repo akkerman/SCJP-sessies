2751724b8f29e6db578a47108a5825b172014643
Marcel Akkerman <marcelakkerman@stedin.net>
2012-08-16 15:57:59 +0200

    Add skeleton + test cases

---
+++ Opdracht 2 - Generic Libraries/Generic Library V00/src/scjp/genericlibrary/Library.java
@@ -0,0 +1,24 @@
+package scjp.genericlibrary;
+
+import java.util.Collection;
+
+public class Library {
+	public Lendable lendItem(String libraryCode) {
+		// TODO implement lendItem(String)
+		return null;
+	}
+
+	public void returnItem(Lendable item) {
+		// TODO implement lendItem(Lendable)
+	}
+
+	public Collection availableItems() {
+		// TODO implement availableItems()
+		return null;
+	}
+
+	public Collection dueItems() {
+		// TODO implement dueItems()
+		return null;
+	}
+}
