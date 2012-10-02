a87ba7dde9179646a7ac47bb410dcf65be063dd9
Marcel Akkerman <marcel.akkerman@pinkroccade.nl>
2012-10-02 11:50:47 +0200

    wijzigen volgorde van print statement

--- Sessie 5 - H6/src/Opdr2.java
+++ Sessie 5 - H6/src/Opdr2.java
@@ -1,24 +1,23 @@
-
-import java.util.Date;
-import java.text.SimpleDateFormat;
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Opdr2 {
 
-	
 	public static void main(String[] args) {
-		
-        //create new java.util.Date object
-        Date date = new Date();
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:ss");
 
-        String strDate = dateFormat.format(date);
-        System.out.print(strDate);
-        
+		// create new java.util.Date object
+		Date date = new Date();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:ss");
+
+		String strDate = dateFormat.format(date);
 		strDate = strDate.substring(0, 10);
+
+		System.out.println(strDate);
 		System.out.print(strDate.toString().length);
-		if("2012-09-27".equalsIgnoreCase(strDate)){
-			System.out.print("today!!");
+
+		if ("2012-09-27".equalsIgnoreCase(strDate)) {
+			System.out.println("today!!");
 		}
 	}
 }
