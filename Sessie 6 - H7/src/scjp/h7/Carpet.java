571793daf15b40ebabf424fbc361b2286a7b3b6b
Marcel Akkerman <marcelakkerman@stedin.net>
2012-10-04 19:58:46 +0200

    final touches

--- Sessie 6 - H7/src/scjp/h7/Carpet.java
+++ Sessie 6 - H7/src/scjp/h7/Carpet.java
@@ -4,7 +4,7 @@ class B extends A { }
 class C extends B { }
 public class Carpet<V extends B> {
 public <X extends V> Carpet<? extends V> method(Carpet<? super X> e) {
-	//	 return new Carpet<X>();
+		 return new Carpet<X>();
 	//	 return new Carpet<V>();
 	//	 return new Carpet<A>();
 	//	 return new Carpet<B>();
--- Sessie 6 - H7/src/scjp/h7/VLA2explained.java
+++ Sessie 6 - H7/src/scjp/h7/VLA2explained.java
@@ -5,19 +5,19 @@ package scjp.h7;
 import java.util.Arrays;  
 import java.util.Comparator;  
   
-public class VLA2 implements Comparator<VLA2> {  
+class VLA2_ implements Comparator<VLA2_> {  
   
     // This class implements Comparator for itself. This  
     // means that anywhere we must pass a parameter of  
-    // Comparator<VLA2>, we can pass an instance of the class.  
+    // Comparator<VLA2_>, we can pass an instance of the class.  
   
     int dishSize;  
   
     public static void main(String[] args) {  
   
-    // create three VLA2 objects with unique dish sizes.  
+    // create three VLA2_ objects with unique dish sizes.  
     // store them in an array named va.  
-    VLA2[] va = { new VLA2(40), new VLA2(200), new VLA2(60) };  
+    VLA2_[] va = { new VLA2_(40), new VLA2_(200), new VLA2_(60) };  
   
     // sort the array using the instance located at array index zero for the comparator  
     // note we could use any of the other instances and get the same result  
@@ -25,14 +25,14 @@ public class VLA2 implements Comparator<VLA2> {
   
     // note the compare method sorts elements in reverse order  
     // so now the array looks like:  
-    // {vla2(200), vla2(60), vla2(40)}  
+    // {VLA2_(200), VLA2_(60), VLA2_(40)}  
   
     // now we are performing a binary search in the array va  
-    // we ask for a VLA2 object whose dish size is 40  
-        // the new VLA2 object is a dummy we pass to the search  
+    // we ask for a VLA2_ object whose dish size is 40  
+        // the new VLA2_ object is a dummy we pass to the search  
         // that holds the attributes we wish to find  
     // finally, we pass the comparator we wish the binary search to use  
-    int index = Arrays.binarySearch(va, new VLA2(40), va[0]);  
+    int index = Arrays.binarySearch(va, new VLA2_(40), va[0]);  
   
     // after the sort, the desired element is at index 2  
     System.out.print(index + " ");  
@@ -46,21 +46,21 @@ public class VLA2 implements Comparator<VLA2> {
     // the element was found in the array. Negative value tells us  
     // it was NOT found.  
   
-    index = Arrays.binarySearch(va, new VLA2(80), va[0]);  
+    index = Arrays.binarySearch(va, new VLA2_(80), va[0]);  
   
-    // there is no VLA2 object in the array with dish size of 80  
+    // there is no VLA2_ object in the array with dish size of 80  
     // the element we wanted to find would be inserted at index 1  
     // so the value of "index" is -2  
     System.out.print(index);  
     }  
   
-    public int compare(VLA2 a, VLA2 b) {  
+    public int compare(VLA2_ a, VLA2_ b) {  
   
         // Sorting in reverse order!  
         return b.dishSize - a.dishSize;  
     }  
   
-    VLA2(int d) {  
+    VLA2_(int d) {  
           dishSize = d;  
     }  
 } 
