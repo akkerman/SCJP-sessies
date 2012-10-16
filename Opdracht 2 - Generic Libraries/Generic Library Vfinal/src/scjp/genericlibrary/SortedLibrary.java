ec7faa725f8f9acd687a98209fe48fba19673cbc
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2012-10-16 21:03:13 +0200

    move SortedLibrary.java

--- Opdracht 2 - Generic Libraries/Generic Library Vfinal/src/scjp/genericlibrary/SortedLibrary.java
+++ Opdracht 2 - Generic Libraries/Generic Library Vfinal/src/scjp/genericlibrary/SortedLibrary.java
@@ -6,7 +6,7 @@ import java.util.TreeSet;
 public class SortedLibrary<C, L extends Lendable<C>> extends Library<C, L> {
 	
 	/**
-	 * Constructs a new Library containing elements in the available and due
+	 * Constructs a new Library containing Lendables. Lendables in the available and due
 	 * collection are sorted according to the natural ordering of its items. All
 	 * Lendables added to this library must implement the Comparable interface.
 	 * Furthermore, all such Lendables must be mutually comparable:
